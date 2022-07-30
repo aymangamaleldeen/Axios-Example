a4467a856b39030d3d2ac10d2453e86c5ad5b31d
aymangamaleldeen <aymangama2009@gmail.com>
2022-07-30 16:55:49 +0200

    get request

--- main.js
+++ main.js
@@ -4,15 +4,7 @@ axios.defaults.headers.common['X-Auth-Token'] =
 
 // GET REQUEST
 function getTodos() {
-  // axios({
-  //   method: 'get',
-  //   url: 'https://jsonplaceholder.typicode.com/todos',
-  //   params: {
-  //     _limit: 5
-  //   }
-  // })
-  //   .then(res => showOutput(res))
-  //   .catch(err => console.error(err));
+
 
   axios
     .get('https://jsonplaceholder.typicode.com/todos?_limit=5', {
@@ -20,6 +12,7 @@ function getTodos() {
     })
     .then(res => showOutput(res))
     .catch(err => console.error(err));
+
 }
 
 // POST REQUEST
